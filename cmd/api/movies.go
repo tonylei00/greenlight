e3d2fa54255a84a269cdbedf175e1a7b53d94c8a
Tony Lei <tony.lei003@gmail.com>
2023-09-20 10:29:29 -0700

    Restructure validation

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"greenlight.tlei.net/internal/data"
+	"greenlight.tlei.net/internal/validator"
 )
 
 // POST /v1/movies
@@ -20,6 +21,20 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 	err := app.readJSON(w, r, &input)
 	if err != nil {
 		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	movie := &data.Movie{
+		Title:   input.Title,
+		Year:    input.Year,
+		Runtime: input.Runtime,
+		Genres:  input.Genres,
+	}
+
+	v := validator.New()
+
+	if data.ValidateMovie(v, movie); !v.IsValid() {
+		app.failedValidationResponse(w, r, v.Errors)
 		return
 	}
 
