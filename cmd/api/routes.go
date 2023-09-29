891219c3d61cb21678e29aee419221a01dda041e
Tony Lei <tony.lei003@gmail.com>
2023-09-28 18:24:53 -0700

    add users

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -23,5 +23,8 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodPatch, "/v1/movies/:id", app.updateMovieHandler)
 	router.HandlerFunc(http.MethodDelete, "/v1/movies/:id", app.deleteMovieHandler)
 
+	// User Routes
+	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
+
 	return app.recoverPanic(app.rateLimit(router))
 }
--- internal/data/movies.go
+++ internal/data/movies.go
@@ -17,13 +17,13 @@ type MovieModel struct {
 }
 
 type Movie struct {
-	ID        int64     `json:"id"`                // Unique integer ID for the movie
-	CreatedAt time.Time `json:"-"`                 // Timestamp for when the movie is added to our database
-	Title     string    `json:"title"`             // Movie title
-	Year      int32     `json:"year,omitempty"`    // Movie release year
-	Runtime   Runtime   `json:"runtime,omitempty"` // Movie runtime (in minutes)
-	Genres    []string  `json:"genres,omitempty"`  // Slice of genres for the movie (romance, comedy, etc.)
-	Version   int32     `json:"version"`           // The version number starts at 1 and will be incremented each time the movie information is updated
+	ID        int64     `json:"id"`
+	CreatedAt time.Time `json:"-"`
+	Title     string    `json:"title"`
+	Year      int32     `json:"year,omitempty"`
+	Runtime   Runtime   `json:"runtime,omitempty"`
+	Genres    []string  `json:"genres,omitempty"`
+	Version   int32     `json:"version"`
 }
 
 func (m MovieModel) Insert(movie *Movie) error {
