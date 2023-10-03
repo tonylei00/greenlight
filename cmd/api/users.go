d0b9f67ea0f5e0b1845ee8997df2a157eb7ef9bb
Tony Lei <tony.lei003@gmail.com>
2023-10-02 17:48:15 -0700

    setup user activation email template

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"net/http"
+	"time"
 
 	"greenlight.tlei.net/internal/data"
 	"greenlight.tlei.net/internal/validator"
@@ -52,8 +53,19 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 		return
 	}
 
+	token, err := app.models.Tokens.New(user.ID, 3*24*time.Hour, data.ScopeActivation)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
 	app.background(func() {
-		err = app.mailer.Send(user.Email, "user_welcome.tmpl", user)
+		tmplData := map[string]any{
+			"activationToken": token.Plaintext,
+			"userID":          user.ID,
+		}
+
+		err = app.mailer.Send(user.Email, "user_welcome.tmpl", tmplData)
 		if err != nil {
 			app.logger.Error(err.Error())
 		}
