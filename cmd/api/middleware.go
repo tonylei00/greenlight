c2ff121c4234c3a2693bef3b4f4df173cb927a2a
Tony Lei <tony.lei003@gmail.com>
2023-10-08 23:43:40 -0700

    finish metrics middleware & start makefile

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"net"
 	"net/http"
+	"strconv"
 	"strings"
 	"sync"
 	"time"
@@ -16,20 +17,67 @@ import (
 	"golang.org/x/time/rate"
 )
 
+// Implements the http.ResponseWriter interface with extra fields to record the response status code
+type metricsResponseWriter struct {
+	/* Field which wraps an existing http.ResponseWriter */
+	wrapped http.ResponseWriter
+
+	/* Field for recording the response status code */
+	statusCode int
+
+	/* Field indicates whether the response headers have already been written */
+	headerWritten bool
+}
+
+func newMetricsResponseWriter(w http.ResponseWriter) *metricsResponseWriter {
+	return &metricsResponseWriter{
+		wrapped: w,
+
+		/* Default status sent by Go in a HTTP response */
+		statusCode: http.StatusOK,
+	}
+}
+
+func (mw *metricsResponseWriter) Header() http.Header {
+	return mw.wrapped.Header()
+}
+
+func (mw *metricsResponseWriter) WriteHeader(statusCode int) {
+	mw.wrapped.WriteHeader(statusCode)
+
+	if !mw.headerWritten {
+		mw.statusCode = statusCode
+		mw.headerWritten = true
+	}
+}
+
+func (mw *metricsResponseWriter) Write(b []byte) (int, error) {
+	mw.headerWritten = true
+	return mw.wrapped.Write(b)
+}
+
+func (mw *metricsResponseWriter) Unwrap() http.ResponseWriter {
+	return mw.wrapped
+}
+
 func (app *application) metrics(next http.Handler) http.Handler {
 	var (
 		totalRequestsReceived           = expvar.NewInt("total_request_received")
 		totalResponsesSent              = expvar.NewInt("total_responses_sent")
 		totalProcessingTimeMicroseconds = expvar.NewInt("total_processing_time_μs")
+		totalResponsesSentByStatus      = expvar.NewMap("total_responses_sent_by_status")
 	)
+
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
 
 		totalRequestsReceived.Add(1)
 
-		next.ServeHTTP(w, r)
+		mw := newMetricsResponseWriter(w)
+		next.ServeHTTP(mw, r)
 
 		totalResponsesSent.Add(1)
+		totalResponsesSentByStatus.Add(strconv.Itoa(mw.statusCode), 1)
 
 		duration := time.Since(start).Microseconds()
 		totalProcessingTimeMicroseconds.Add(duration)
